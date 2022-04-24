7af011eedeb1c3c88c4b8a2b7be2930915c690b2
sgzmd <sigizmund@gmail.com>
2022-04-24 09:12:19 +0100

    Running downloaded on first start, in a volume

--- data/downloader_launcher.sh
+++ data/downloader_launcher.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-/app/create_sqlite_file.sh -f /app/flibusta.db -d /app -t $telegram_api_key -c $telegram_chat_id
+/app/create_sqlite_file.sh -f /var/local/flibudata/flibusta.db -d /app -t $telegram_api_key -c $telegram_chat_id
+/app/flibustier_server --flibusta_db=/var/local/flibudata/flibusta.db --port=9000
