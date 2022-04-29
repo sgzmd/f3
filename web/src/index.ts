12b274d33d8a6604879b05ad2ef44be48bb62825
sgzmd <sigizmund@gmail.com>
2022-04-29 14:07:37 +0100

    index.ts can speak to grpc server

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,10 +1,15 @@
-import {SearchRequest} from '../gen/flibuserver/proto/flibustier_pb';
+import {GlobalSearchRequest} from '../gen/nodejs/flibuserver/proto/v1/flibustier_pb';
+import {FlibustierServiceClient} from "../gen/nodejs/flibuserver/proto/v1/flibustier_grpc_pb";
+import * as grpc from '@grpc/grpc-js';
+
 
 const world = 'world';
 
 export function hello(world: string = "world"): string {
-  var sr = new SearchRequest();
-  sr.setSearchTerm("Test");
+  var sr = new GlobalSearchRequest();
+  sr.setSearchTerm("Маск");
+
+  var client = new FlibustierServiceClient("172.23.22.238:9000", grpc.credentials.createInsecure() );
 
   return JSON.stringify(sr.toObject());
 }
