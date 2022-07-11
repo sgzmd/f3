53b653b41b76679ebe9a116a0a0f8492544ab87a
sgzmd <sigizmund@gmail.com>
2022-07-11 12:32:15 +0100

    a

--- web/templates/static/flibustier.js
+++ web/templates/static/flibustier.js
@@ -1,4 +1,8 @@
 function onSearchSubmit() {
     let searchTerm = document.getElementById("searchTerm").value;
     window.location = "/search/" + searchTerm;
+    return false;
 }
+
+
+// Generates onSubmit for HTML form
