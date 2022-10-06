26448554b72d040ec1bb4744567db406d9dbca3a
sgzmd <sigizmund@gmail.com>
2022-10-06 15:58:06 +0100

    Using mysql2sqlite python script for sqlite3 conversion.
    This works much better because now we are using the official interface rather than trying to
    hack the dump file.

--- data/cmd/downloader/main.go
+++ data/cmd/downloader/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"compress/gzip"
 	"flag"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
 	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/sgzmd/f3/data/helpers"
 
@@ -54,18 +59,83 @@ func main() {
 	const BASE_URL = "http://flibusta.site/sql/"
 	baseUrl := flag.String("base_url", BASE_URL, "Base URL of the Flibusta SQL download page")
 	download := flag.Bool("download", true, "Whether new files are to be downloaded")
+	dump := flag.String("dump_file", "flibusta.sql", "Name of the dump file to be created")
+
+	flag.Parse()
 
 	if *download {
+		log.Printf("Downloading from %s to %s", *baseUrl, *dump)
 		urls := CreateUrlList(*baseUrl)
 
-		for _, u := range urls {
-			parsedUrl, err := url.Parse(u)
-			if err != nil {
-				panic(err)
-			}
+		var wg sync.WaitGroup
 
-			fileName := filepath.Base(parsedUrl.Path)
-			helpers.DownloadFile(fileName, u)
+		gzippedFiles := make([]string, 0)
+
+		sqldump, err := os.Create(*dump)
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		var mu sync.Mutex
+		for _, u := range urls {
+			wg.Add(1)
+			go func(u string) {
+				defer wg.Done()
+
+				// Detecting the name of the file and creating the temp file
+				parsedUrl, err := url.Parse(u)
+				if err != nil {
+					log.Fatal(err)
+				}
+				tempFile, err := ioutil.TempFile("", filepath.Base(parsedUrl.Path))
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer os.Remove(tempFile.Name())
+				filename := tempFile.Name()
+
+				log.Printf("Starting download from %s -> %s", u, filename)
+				err = helpers.DownloadFile(filename, u)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Printf("%s -> %s finished.", u, filename)
+				gzippedFiles = append(gzippedFiles, filename)
+
+				f, err := os.Open(filename)
+				if err != nil {
+					log.Fatal(err)
+				}
+				gzr, err := gzip.NewReader(f)
+				if err != nil {
+					log.Fatal(err)
+				}
+				bytes, err := ioutil.ReadAll(gzr)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				mu.Lock()
+				defer mu.Unlock()
+
+				n, err := sqldump.Write(bytes)
+				if n != len(bytes) {
+					log.Printf("Written only %d bytes out of %d to %s", n, len(bytes), *dump)
+				}
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(u)
+		}
+
+		wg.Wait()
+
+		err = sqldump.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		os.Chmod(*dump, fs.ModePerm)
 	}
 }
--- data/create_sqlite_file.sh
+++ data/create_sqlite_file.sh
@@ -30,11 +30,6 @@ optstring="d:t:c:f:"
 active_dir="."
 telegram_key=""
 telegram_chat_id=""
-mariadb_database=""
-mariadb_password=""
-mariadb_user=""
-mariadb_host=""
-mariadb_port=""
 flibusta_db_path="none.db"
 
 while getopts ${optstring} arg; do
@@ -81,38 +76,22 @@ echo "Will be creating $flibusta_db_path"
 # assert_exists sqlite3
 
 # Downloading the files, this might take a while...
-./downloader
+./downloader --dump_file flibusta_mysql_dump.sql
 
 if [ $? -ne 0 ]; then
     send_update $telegram_key $telegram_chat_id "File downloader has failed with error code $?"
     exit 11
 fi
 
-# Unpacking files
-gunzip *.sql.gz
+mysql --host database -uflibustier -pflibusta flibustier < flibusta_mysql_dump.sql
 
-# Creating combined SQL dump file
-cat lib*.sql > flibusta_mysql_dump.sql
-
-mysqldump -v -u$mariadb_user -p$mariadb_password $mariadb_database < flibusta_mysql_dump.sql
-
-# Applying MySQL -> sqlite3 converter script
-awk -f mysql2sqlite flibusta_mysql_dump.sql > flibusta_sqlite_dump.sql
-if [ $? -ne 0 ]; then
-    send_update $telegram_key $telegram_chat_id "mysql2sqlite has failed with error code $?"
-    exit 2
-fi
-
-# Removing old database copy if any
 rm -rf flibusta_new.db
+mysql2sqlite -f flibusta_new.db \
+  --mysql-database flibustier \
+  --mysql-user flibustier \
+  --mysql-host database \
+  --mysql-password flibusta
 
-# Converting Flibusta SQLite3 dump to SQLite3 DB
-sqlite3 flibusta_new.db < flibusta_sqlite_dump.sql
-
-if [ $? -ne 0 ]; then
-    send_update $telegram_key $telegram_chat_id "sqlite3 db creation has failed with error code $?"
-    exit 3
-fi
 
 # Applying necessary patches to the DB
 sqlite3 flibusta_new.db < SequenceAuthor.sql
