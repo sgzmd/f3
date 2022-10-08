0c98a44b8b668d2349c369a9f4601715aa75089e
sgzmd <sigizmund@gmail.com>
2022-10-08 11:58:22 +0100

    Passing variables as args

--- data/scripts/docker_download.sh
+++ data/scripts/docker_download.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "PRODUCTION run of downloader"
-/app/create_sqlite_file.sh -f /var/local/flibudata/flibusta.db -d /app -t $telegram_api_key -c $telegram_chat_id
+#/app/create_sqlite_file.sh -f /var/local/flibudata/flibusta.db -d /app -t
+#${TELEGRAM_API_KEY} -c ${TELEGRAM_CHAT_ID}
+/app/scripts/init-database.py \
+  --telegram_api_key=${TELEGRAM_API_KEY} \
+  --telegram_chat_id=${TELEGRAM_CHAT_ID} \
+  --create_sqlite_file=/var/local/flibudata/flibusta.db
