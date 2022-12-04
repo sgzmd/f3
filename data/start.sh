7156148278cc94bdf606d6a1cfe6ac9e50292d65
sgzmd <sigizmund@gmail.com>
2022-12-04 15:05:37 +0000

    Fixing start.sh to include correct path to updater script

--- data/start.sh
+++ data/start.sh
@@ -3,5 +3,9 @@
 set -x
 
 bash /app/scripts/docker_download.sh
-/app/flibustier_server --flibusta_db=/var/local/flibudata/flibusta.db --datastore=/var/local/flibudata/datastore.badger --port=9000 --update_cmd=./docker_download.sh --update_every=24h
+/app/flibustier_server --flibusta_db=/var/local/flibudata/flibusta.db \
+  --datastore=/var/local/flibudata/datastore.badger \
+  --port=9000 \
+  --update_cmd=/app/scripts/docker_download.sh \
+  --update_every=24h
 
