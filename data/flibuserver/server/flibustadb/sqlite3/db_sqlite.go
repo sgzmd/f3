70d60d4f5a8ea27fa2c60f3b17182b7d619a925c
sgzmd <sigizmund@gmail.com>
2023-01-07 19:05:12 +0000

    Note that overall tests are failing which is not good

--- data/flibuserver/server/flibustadb/sqlite3/db_sqlite.go
+++ data/flibuserver/server/flibustadb/sqlite3/db_sqlite.go
@@ -3,6 +3,7 @@ package sqlite3
 import (
 	"database/sql"
 	pb "github.com/sgzmd/f3/data/gen/go/flibuserver/proto/v1"
+	"log"
 	"sync"
 )
 
@@ -11,7 +12,7 @@ const (
 )
 
 // Implements FlibustaDb interface for sqlite3 database.
-type Sqlite3Db struct {
+type Sqlite3Database struct {
 	sqliteDb        *sql.DB
 	authorStatement *sql.Stmt
 	seriesStatement *sql.Stmt
@@ -19,17 +20,19 @@ type Sqlite3Db struct {
 }
 
 // SearchAuthors searches authors by name.
-func (s *Sqlite3Db) SearchAuthors(req *pb.GlobalSearchRequest) ([]*pb.FoundEntry, error) {
-	return nil, nil
+func (s *Sqlite3Database) SearchAuthors(req *pb.GlobalSearchRequest) ([]*pb.FoundEntry, error) {
+	query := CreateAuthorSearchQuery(req.SearchTerm)
+	return s.iterateOverAuthors(query)
 }
 
 // SearchSeries searches series by name.
-func (s *Sqlite3Db) SearchSeries(req *pb.GlobalSearchRequest) ([]*pb.FoundEntry, error) {
-	return nil, nil
+func (s *Sqlite3Database) SearchSeries(req *pb.GlobalSearchRequest) ([]*pb.FoundEntry, error) {
+	query := CreateSequenceSearchQuery(req.SearchTerm)
+	return s.iterateOverSeries(query)
 }
 
 // GetAuthorBooks returns all books by author.
-func (s *Sqlite3Db) GetAuthorBooks(authorId int64) ([]*pb.Book, error) {
+func (s *Sqlite3Database) GetAuthorBooks(authorId int64) ([]*pb.Book, error) {
 	{
 		// Scoped lock which will be unlocked whether we create a new statement or not.
 		s.lock.Lock()
@@ -63,7 +66,7 @@ func (s *Sqlite3Db) GetAuthorBooks(authorId int64) ([]*pb.Book, error) {
 }
 
 // GetSeriesBooks queries sqlite3 database for books in series.
-func (s *Sqlite3Db) GetSeriesBooks(seriesId int64) ([]*pb.Book, error) {
+func (s *Sqlite3Database) GetSeriesBooks(seriesId int64) ([]*pb.Book, error) {
 	{
 		// Scoped lock which will be unlocked whether we create a new statement or not.
 		s.lock.Lock()
@@ -96,7 +99,72 @@ func (s *Sqlite3Db) GetSeriesBooks(seriesId int64) ([]*pb.Book, error) {
 	return books, nil
 }
 
-// NewSqlite3Db creates new Sqlite3Db instance.
-func NewSqlite3Db(sqliteDb *sql.DB) *Sqlite3Db {
-	return &Sqlite3Db{sqliteDb: sqliteDb, authorStatement: nil, seriesStatement: nil}
+// NewSqlite3Db creates new Sqlite3Database instance.
+func NewSqlite3Db(sqliteDb *sql.DB) *Sqlite3Database {
+	return &Sqlite3Database{sqliteDb: sqliteDb, authorStatement: nil, seriesStatement: nil}
+}
+
+func (s *Sqlite3Database) iterateOverAuthors(query string) ([]*pb.FoundEntry, error) {
+	rows, err := s.sqliteDb.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var entries []*pb.FoundEntry = make([]*pb.FoundEntry, 0, 10)
+	for rows.Next() {
+		var authorName string
+		var authorId int64
+		var count int32
+
+		err = rows.Scan(&authorName, &authorId, &count)
+		if err != nil {
+			log.Fatalf("Failed to scan the row: %v", err)
+			return nil, err
+		}
+
+		entries = append(entries, &pb.FoundEntry{
+			EntryType:   pb.EntryType_ENTRY_TYPE_AUTHOR,
+			Author:      authorName,
+			EntryName:   authorName,
+			EntryId:     authorId,
+			NumEntities: count,
+		})
+	}
+
+	return entries, nil
+}
+
+func (s *Sqlite3Database) iterateOverSeries(query string) ([]*pb.FoundEntry, error) {
+	rows, err := s.sqliteDb.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var entries []*pb.FoundEntry = make([]*pb.FoundEntry, 0, 10)
+	for rows.Next() {
+		var seqName string
+		var authors string
+		var seqId int64
+		var count int32
+
+		err = rows.Scan(&seqName, &authors, &seqId, &count)
+		if err != nil {
+			log.Fatalf("Failed to scan the row: %v", err)
+			return nil, err
+		}
+
+		entries = append(entries, &pb.FoundEntry{
+			EntryType:   pb.EntryType_ENTRY_TYPE_SERIES,
+			Author:      authors,
+			EntryName:   seqName,
+			EntryId:     seqId,
+			NumEntities: count,
+		})
+	}
+
+	return entries, nil
 }
