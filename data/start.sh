3fb36614c906e72388dcbeadb57260e59248b394
sgzmd <sigizmund@gmail.com>
2023-02-28 19:21:37 +0000

    Adding mysql parameters to go program

--- data/start.sh
+++ data/start.sh
@@ -7,5 +7,11 @@ bash /app/scripts/docker_download.sh
   --datastore=/var/local/flibudata/datastore.badger \
   --port=9000 \
   --update_cmd=/app/scripts/docker_download.sh \
-  --update_every=24h
+  --update_every=24h \
+  --mysql_host=${MARIADB_HOST} \
+  --mysql_user=${MARIADB_USER} \
+  --mysql_pass=${MARIADB_PASSWORD} \
+  --mysql_db=${MARIADB_DATABASE} \
+  --mysql_port=${MARIADB_PORT}
+
 
