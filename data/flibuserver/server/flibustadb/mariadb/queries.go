3843199870382050365229d6ce77f530aaeb0179
sgzmd <sigizmund@gmail.com>
2023-03-04 14:03:38 +0000

    Finishing Maria migration and adding integration test

--- data/flibuserver/server/flibustadb/mariadb/queries.go
+++ data/flibuserver/server/flibustadb/mariadb/queries.go
@@ -6,7 +6,7 @@ import (
 )
 
 const SearchAuthorsFtsMysql = `
-select lan.FirstName, lan.MiddleName, lan.LastName, la.AvtorId, COUNT(1) num
+select concat_name(lan.FirstName, lan.MiddleName, lan.LastName) authorName, la.AvtorId, COUNT(1) num
 from libavtor la,
      libavtorname lan,
      libbook lb
@@ -14,8 +14,24 @@ where la.AvtorId = lan.AvtorId
   and la.BookId = lb.BookId
   and lb.Deleted != '1'
   and match(lan.FirstName, lan.LastName, lan.MiddleName) against('%s*' in boolean mode)
-group by 1, 2, 3
+group by 1, 2
 order by num desc;`
+
+const SearchSeriesFtsMysql = `
+select lsn.SeqId,
+       SeqName,
+       GROUP_CONCAT(DISTINCT concat_name(FirstName, MiddleName, LastName) order by LastName ) as AuthorName,
+       (select count(ls.BookId) from libseq ls where ls.SeqId = lsn.SeqId)         NumBooks
+from libseqname lsn,
+     libseq ls,
+     libavtor la,
+     libavtorname lan
+where lsn.SeqId = ls.SeqId
+  and ls.BookId = la.BookId
+  and la.AvtorId = lan.AvtorId
+  and match(lsn.SeqName) against('%s*' in boolean mode)
+GROUP BY lsn.SeqId
+order by (select count(ls.BookId) from libseq ls where ls.SeqId = lsn.SeqId) desc;`
 
 func MakeAuthorName(firstName, middleName, lastName string) string {
 	return strings.TrimSpace(fmt.Sprintf("%s, %s %s", lastName, firstName, middleName))
--- data/go.mod
+++ data/go.mod
@@ -25,12 +25,13 @@ require (
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/flatbuffers v1.12.1 // indirect
 	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/jessevdk/go-flags v1.5.0 // indirect
 	github.com/klauspost/compress v1.12.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.opencensus.io v0.22.5 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/sys v0.0.0-20221010170243-090e33056c14 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/genproto v0.0.0-20220317150908-0efb43f6373e // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
