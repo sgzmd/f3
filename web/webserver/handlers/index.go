9c8e64379a82d045e4a2e609925e866f89819cb1
sgzmd <sigizmund@gmail.com>
2023-03-04 17:17:24 +0000

    Sorting by last saved timestamp

--- web/webserver/handlers/index.go
+++ web/webserver/handlers/index.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/sgzmd/go-telegram-auth/tgauth"
+	"sort"
 )
 
 func IndexHandler(client ClientContext) func(c *fiber.Ctx) error {
@@ -11,6 +12,10 @@ func IndexHandler(client ClientContext) func(c *fiber.Ctx) error {
 		ui := c.Locals("user")
 		userInfo := ui.(*tgauth.UserInfo)
 		sr, err := GetTrackedEntries(client, userInfo)
+
+		sort.Slice(sr, func(i, j int) bool {
+			return sr[i].Entry.Saved.Seconds > sr[j].Entry.Saved.Seconds
+		})
 
 		if err != nil {
 			return c.Status(500).SendString(err.Error())
